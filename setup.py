f5936d1b4223d560c25b85e2cf9630bd5feb5db4
Ruisi Zhang <ruisizhang123@gmail.com>
2024-10-23 02:08:17 +0000

    work on mast

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     author="PyTorch Team",
     url="https://github.com/pytorch/benchmark",
     packages=find_packages(include=["torchbenchmark*", "userbenchmark*"]),
+    package_data={
+        'torchbenchmark': ['**/*.txt'],
+        'userbenchmark': ['**/*.txt'],   
+    },
+    include_package_data=True,
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
